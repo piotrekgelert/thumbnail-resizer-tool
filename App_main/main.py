dd036eb7ecb83ba706f75ca62652955fae911cc6
PiotrekGelert <piotrekgelert@gmail.com>
2023-12-25 15:10:09 +0000

    working version of app, block image sizes over img size work in progress

--- App_main/main.py
+++ App_main/main.py
@@ -6,47 +6,79 @@ from PIL import Image
 from PyQt6 import QtCore as qtc
 from PyQt6 import QtGui as qtg
 from PyQt6 import QtWidgets as qtw
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QWidget
 from UI.untitled_ui import Ui_fm_main
 
 
 class ThumbResizer(qtw.QWidget, Ui_fm_main):
-    sizes_dict = {'16px':16, '32px':32, '64px':64, '128px':128,'256px': 256,'512px': 512}
+    sizes_dict = {
+        '16px':16, '32px':32, '64px':64, '128px':128,'256px': 256,'512px': 512
+        }
+    image_size = 0
+    pathh, save_pathh = '', ''
     
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.cb_sizes.addItems([x for x in self.sizes_dict.keys()])
+        self.cb_sizes.addItems([k for k in self.sizes_dict.keys()])
         self.pb_resize.clicked.connect(self.image_workout)
         self.pb_cancel.clicked.connect(self.close)
         self.pb_select_img_path.clicked.connect(self.path_to_file)
         self.pb_select_folder_path.clicked.connect(self.path_to_folder)
 
+    def image_open(self):
+        pass
+
+    
     def image_workout(self):
-        pathh = self.pb_select_img_path.text()
-        save_pathh = self.pb_select_folder_path.text()
-        image_name = pathh.split('\\')[-1].split('.')[0]
-        curr_size = self.sizes_dict[self.cb_sizes.currentText()]
-        with Image.open(pathh) as im:
+        image_name, num, image_extension = self.img_name_extension(self.pathh)
+        
+        with Image.open(self.pathh) as im:
+            self.image_size = im.size[0]
+            curr_size = self.sizes_dict[self.cb_sizes.currentText()]
             img = im.resize((curr_size, curr_size), Image.Resampling.LANCZOS)
-            img.save(os.path.join(save_pathh, image_name+'_thumb.png'))
+            img_name = image_name+f'_thumb.{image_extension}'
+            
+            if self.already_exists(self.save_pathh, img_name):
+                img.save(
+                os.path.join(
+                    self.save_pathh,
+                    image_name+f'{num+1}_thumb.{image_extension}'
+                    )
+                )
+            else:
+                img.save(
+                    os.path.join(
+                        self.save_pathh, img_name
+                        )
+                    )
     
     def path_to_file(self):
         file_path = qtw.QFileDialog.getOpenFileName()
+        self.pathh = file_path[0]
         self.lb_selected_img_path.setText(file_path[0])
-        self.check_img_in_folder(file_path[0])
 
     def path_to_folder(self):
         folder_path = qtw.QFileDialog.getExistingDirectory()
+        self.save_pathh = folder_path
         self.lb_selected_folder_path.setText(folder_path)
-        
     
-    def check_img_in_folder(self, pth):
-        print(pth)
+    def img_name_extension(self, p:str):
+        img_name, img_extension = p.split('/')[-1].split('.')
+        nums = [x for x in img_name if x.isdigit()]
+        if len(nums):
+            return img_name, int(''.join(nums)), img_extension
+        else:
+            return img_name, 0, img_extension
+    
+    def already_exists(self, p:str, file):
+        return file in os.listdir(p)
+    
+    def check_icon_sizes(self):
+        self.cb_sizes.addItems(
+            [k for k, v in self.sizes_dict.items() if v < self.image_size]
+            )
 
         
-
 
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # thumbnail-resizer-tool
-Tool to resize PNG image to its very small version made with PIL library and pyqt.
+Tool to resize icon to its very small version made with PIL library and pyqt.
 
 
 
-checks if resized image already exists, if yes adds to name number
-by default name is taken from original image and to resized img name '_thumb' is added
-image resizer saves image as PNG files
-sizes to select are: 16px, 32px, 64px, 128px,256px, 512px
+checks if resized icon already exists, if yes adds to name number
+name is taken from original icon and to resized icon name '_thumb' is added
+icon resizer saves files with original extention
+sizes to select are: 16px, 32px, 64px, 128px, 256px, 512px
 
 <a href="https://www.flaticon.com/free-icons/cms" title="cms icons">Cms icons created by juicy_fish - Flaticon</a>
 <a href="https://www.flaticon.com/free-icons/screenshot" title="screenshot icons">Screenshot icons created by juicy_fish - Flaticon</a>
